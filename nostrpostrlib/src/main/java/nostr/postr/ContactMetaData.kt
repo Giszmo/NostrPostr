e82c1ea8b0fe46aad8a5f6ea7279043f61b1aaa3
Leo Wandersleb <leo@leowandersleb.de>
2022-07-01 20:13:12 -0400

    minor improvements

--- nostrpostrlib/src/main/java/nostr/postr/ContactMetaData.kt
+++ nostrpostrlib/src/main/java/nostr/postr/ContactMetaData.kt
@@ -1,3 +1,7 @@
 package nostr.postr
 
-data class ContactMetaData(val name: String, val picture: String, val about: String, val nip05: String?)
+data class ContactMetaData(
+    val name: String,
+    val picture: String,
+    val about: String,
+    val nip05: String?)
